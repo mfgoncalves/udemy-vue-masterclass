93dfd67cfbd9012c79c11cd001ddefb97084efbf
Marcelo Gonçalves <marcelo.goncalves@objectedge.com>
2022-05-13 10:53:54 -0300

    90. login button and profile image

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button
+    class="px-4 py-2 font-medium text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
+    @click="handleClick"
+  >
+    Sign in
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+};
+</script>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -22,14 +22,22 @@
             </li>
           </ul>
         </nav>
+        <div class="flex items-center h-full ml-auto">
+          <ProfileImage v-if="isLoggedIn" data-test="profile-image" />
+          <ActionButton v-else data-test="login-button" @click="loginUser" />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue";
+import ProfileImage from "@/components/ProfileImage.vue";
+
 export default {
   name: "MainNav",
+  components: { ActionButton, ProfileImage },
   data() {
     return {
       company: "OE Careers",
@@ -42,7 +50,13 @@ export default {
         "Students",
         "Jobs",
       ],
+      isLoggedIn: false,
     };
+  },
+  methods: {
+    loginUser() {
+      this.isLoggedIn = true;
+    },
   },
 };
 </script>
