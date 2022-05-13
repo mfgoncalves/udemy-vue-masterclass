2b9b2816bd5066632455c7d81191ce4f8f736dc6
Marcelo Gonçalves <marcelo.goncalves@objectedge.com>
2022-05-13 11:09:17 -0300

    90. last commit without coverage folder

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -22,4 +22,26 @@ describe("MainNav", () => {
       "Jobs",
     ]);
   });
+
+  describe("when user is logged out", () => {
+    it("prompts user to sign in", () => {
+      const wrapper = mount(MainNav);
+      const loginButton = wrapper.find("[data-test='login-button']");
+      expect(loginButton.exists()).toBeTruthy();
+    });
+  });
+
+  describe("when user is logged in", () => {
+    it("displays user profile picture", async () => {
+      const wrapper = mount(MainNav);
+      let profileImage = wrapper.find("[data-test='profile-image']");
+      expect(profileImage.exists()).toBeFalsy();
+
+      const loginButton = wrapper.find("[data-test='login-button']");
+      await loginButton.trigger("click");
+
+      profileImage = wrapper.find("[data-test='profile-image']");
+      expect(profileImage.exists()).toBeTruthy();
+    });
+  });
 });
