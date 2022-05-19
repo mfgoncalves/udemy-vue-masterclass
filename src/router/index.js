cca58ef8ea7368632e7d23f98d7b294a97daa0bc
Marcelo Gonçalves <marcelo.goncalves@objectedge.com>
2022-05-19 14:40:03 -0300

    201. adds Job Listing screen and implements lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
-import JobResultsView from "@/views/JobResultsView.vue";
+
+// Lazy loading
+const HomeView = () => import("@/views/HomeView.vue");
+const JobResultsView = () =>
+  import(/* webpackChunkName: "jobs" */ "@/views/JobResultsView.vue");
+const JobView = () =>
+  import(/* webpackChunkName: "jobs" */ "@/views/JobView.vue");
 
 const routes = [
   {
@@ -12,6 +17,11 @@ const routes = [
     path: "/jobs/results",
     name: "JobResults",
     component: JobResultsView,
+  },
+  {
+    path: "/jobs/results/:id",
+    name: "JobLising",
+    component: JobView,
   },
 ];
 
---
+++ src/views/JobView.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>JobPage id: {{ currentJobId }}</h1>
+</template>
+
+<script>
+export default {
+  name: "JobView",
+  computed: {
+    currentJobId() {
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
