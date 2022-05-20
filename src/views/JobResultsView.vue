510798b2ea6b7439aeb44ae2aa99da76a33353bf
Marcelo Gonçalves <marcelo.goncalves@objectedge.com>
2022-05-20 10:14:46 -0300

    216. adds job listing card

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>a</div>
+  <div class="flex flex-row flex-nowrap w-full">
+    <JobFiltersSidebar />
+    <JobListings />
+  </div>
 </template>
 
 <script>
+import JobFiltersSidebar from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue";
+import JobListings from "@/components/JobResults/JobListings.vue";
+
 export default {
   name: "JobResultsView",
-  methods: {
-    goToHome() {
-      this.$router.push({ name: "Home" });
-    },
-  },
+  components: { JobFiltersSidebar, JobListings },
 };
 </script>
