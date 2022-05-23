b49ddba4d30f3385ad597850c028a66fc075a6f4
Marcelo Gonçalves <marcelo.goncalves@objectedge.com>
2022-05-23 09:57:35 -0300

    254. adds vuex

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import { createApp } from "vue";
 import App from "@/App.vue";
 import "@/assets/tailwind.css";
-
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import router from "@/router";
+import store from "@/store";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import router from "@/router";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { createApp } from "vue";
 
 library.add(faSearch);
 
 createApp(App)
+  .use(store)
   .use(router)
   .component("FontAwesomeIcon", FontAwesomeIcon)
   .mount("#app");
