0012b000766bca14fc6be99d4fa225732aa1ca10
Marcelo Gonçalves <marcelo.goncalves@objectedge.com>
2022-06-02 15:09:37 -0300

    438. adds typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-recommended", "eslint:recommended", "prettier"],
+
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: "@typescript-eslint/parser",
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": "off",
   },
+
   overrides: [
     {
       files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
@@ -19,5 +22,12 @@ module.exports = {
         jest: true,
       },
     },
+  ],
+
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "prettier",
+    "@vue/typescript",
   ],
 };
